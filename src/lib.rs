78314a75fb5e1279aeefea86b7589a42cf329462
Carlos González <104286054+Fudyy@users.noreply.github.com>
2024-02-23 14:34:45 -0300

    Add unit tests for Banner module

--- src/lib.rs
+++ src/lib.rs
@@ -87,3 +87,21 @@ impl Banner {
         DynamicImage::ImageRgba8(self.image.clone())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::Banner;
+
+    #[test]
+    fn test_banner() {
+        let mut banner = Banner::new(crate::colors::MCColor::Green);
+        banner.render();
+    }
+
+    #[test]
+    fn test_banner_with_pattern() {
+        let mut banner = Banner::new(crate::colors::MCColor::Green);
+        banner.add_pattern(crate::patterns::Pattern::Bricks, crate::colors::MCColor::Red);
+        banner.render();
+    }
+}
