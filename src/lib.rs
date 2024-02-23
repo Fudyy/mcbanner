ad491bb2c6eeb3d9a424a2f2c373b666a27ffe5d
Carlos González <104286054+Fudyy@users.noreply.github.com>
2024-02-23 15:59:54 -0300

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//! # Overview
+//! This library is a simple way to create banners from Minecraft. It uses the [image](https://docs.rs/image/) crate under the hood to generate the images.
+//!
+//! ## Usage
+//! The [Banner] struct is the main entrypoint of the library.
+//! ```rust,no_run
+//! use mcbanner::{Banner, Pattern, MCColor};
+//! 
+//! fn main() {
+//!     let mut banner = Banner::new(MCColor::Red);
+//!     banner.add_pattern(Pattern::Bricks, MCColor::Orange);
+//!     banner.render();
+//!     banner.save("banner.png").unwrap();
+//! }
+
 mod banner;
 mod patterns;
 mod colors;
@@ -12,14 +27,15 @@ mod test {
 
     #[test]
     fn test_banner() {
-        let mut banner = Banner::new(crate::colors::MCColor::Green);
+        let mut banner = Banner::new(crate::colors::MCColor::Red);
         banner.render();
     }
 
     #[test]
     fn test_banner_with_pattern() {
-        let mut banner = Banner::new(crate::colors::MCColor::Green);
-        banner.add_pattern(crate::patterns::Pattern::Bricks, crate::colors::MCColor::Red);
+        let mut banner = Banner::new(crate::colors::MCColor::Lime);
+        banner.add_pattern(crate::patterns::Pattern::Creeper, crate::colors::MCColor::Black);
         banner.render();
+        banner.save("banner.png").unwrap();
     }
 }
